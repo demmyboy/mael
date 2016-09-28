32198e498e0b7b126235e3f94a3451acca066bf6
Demmy <demolaabdulai@live.com>
2016-09-28 12:30:32 +0200

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -72,7 +72,21 @@ jQuery(document).ready(function() {
         }
      });
 
-      
-     
+     /*// Hide the extra content initially, using JS so that if JS is disabled, no problemo:
+        $('.read-more-content').addClass('hide')
+        $('.read-more-show, .read-more-hide').removeClass('hide')
 
+        // Set up the toggle effect:
+        $('.read-more-show').on('click', function(e) {
+          $(this).next('.read-more-content').removeClass('hide');
+          $(this).addClass('hide');
+          e.preventDefault();
+        });
+
+        $('.read-more-hide').on('click', function(e) {
+          $(this).parent('.read-more-content').addClass('hide');
+          var moreid=$(this).attr("more-id");
+          $('.read-more-show#'+moreid).removeClass('hide');
+          e.preventDefault();
+        });*/
 });
